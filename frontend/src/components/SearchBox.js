404062e0049531736a723c5af44067dbcf072486
Md <feroze02@hotmail.com>
2022-01-21 04:39:32 +0000

    Prepare for deployment

---
+++ frontend/src/components/SearchBox.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+
+const SearchBox = ({ history }) => {
+  const [keyword, setKeyword] = useState('')
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (keyword.trim()) {
+      history.push(`/search/${keyword}`)
+    } else {
+      history.push('/')
+    }
+  }
+
+  return (
+    <Form 
+    className='sb'
+    onSubmit={submitHandler} inline>
+      <Form.Control
+        type='text'
+        name='q'
+        onChange={(e) => setKeyword(e.target.value)}
+        placeholder='Search Products...'
+      ></Form.Control>
+       <Button type='submit' variant='dark' className='p-1.5'>
+        Search
+      </Button>
+    </Form>
+  )
+}
+
+export default SearchBox
